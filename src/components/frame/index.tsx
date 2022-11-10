a4a8ec53e81244eca8c5d72e8347dd91951f801b
ulnk <ulnk@skid.today>
2022-11-10 22:41:18 +0000

    fix readme

--- src/components/frame/index.tsx
+++ src/components/frame/index.tsx
@@ -1,18 +1,30 @@
-import React from 'react';
-import { appWindow } from '@tauri-apps/api/window';
+import React from "react";
+import { appWindow } from "@tauri-apps/api/window";
 
-import { VscChromeClose, VscChromeMaximize, VscChromeMinimize } from 'react-icons/vsc';
-import './index.css';
+import {
+  VscChromeClose,
+  VscChromeMaximize,
+  VscChromeMinimize,
+} from "react-icons/vsc";
+import "./index.css";
 
 const Frame = () => {
-    return (<div data-tauri-drag-region className="frame">
-        <span data-tauri-drag-region>von launcher</span>
-        <ul>
-            <button onClick={() => appWindow.minimize()}><VscChromeMinimize /></button>
-            <button onClick={() => appWindow.toggleMaximize()}><VscChromeMaximize /></button>
-            <button onClick={() => appWindow.close()} className='close'><VscChromeClose /></button>
-        </ul>
-    </div>);
-}
+  return (
+    <div data-tauri-drag-region className="frame">
+      <span data-tauri-drag-region>von</span>
+      <ul>
+        <button onClick={() => appWindow.minimize()}>
+          <VscChromeMinimize />
+        </button>
+        <button onClick={() => appWindow.toggleMaximize()}>
+          <VscChromeMaximize />
+        </button>
+        <button onClick={() => appWindow.close()} className="close">
+          <VscChromeClose />
+        </button>
+      </ul>
+    </div>
+  );
+};
 
 export default Frame;
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,19 +1,3 @@
 /// <reference types="vite/client" />
 
-type Applications = {
-  all: Application[];
-};
-
-type Action = {
-  type: string;
-  payload: any;
-};
-
-type Application = {
-  path: string;
-  name: string;
-  valid: boolean;
-  artworkPath?: string;
-  artwork?: string;
-  gradient?: boolean;
-};
+type AppState = {};
